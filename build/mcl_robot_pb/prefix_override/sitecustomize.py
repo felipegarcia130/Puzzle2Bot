d1f980d6f71b60dd3f584ca604193fc0cc33ee1d
felipegarcia130 <felisuper13@hotmail.com>
2026-06-03 13:45:22 -0600

    cambios de paqueteria

---
+++ build/mcl_robot_pb/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/felipe/puzzlebot_ws/install/mcl_robot_pb'
--- src/obstacle_avoidance/setup.py
+++ src/obstacle_avoidance/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'obstacle_avoidance'
+package_name = 'obstacle_avoidance_pb'
 
 setup(
     name=package_name,
@@ -24,10 +24,10 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'avoider_node=obstacle_avoidance.avoider_node:main',
-            'bug0=obstacle_avoidance.bug0:main',
-            'bug1=obstacle_avoidance.bug1:main',
-            'bug2=obstacle_avoidance.bug2:main',
+            'avoider_node=obstacle_avoidance_pb.avoider_node:main',
+            'bug0=obstacle_avoidance_pb.bug0:main',
+            'bug1=obstacle_avoidance_pb.bug1:main',
+            'bug2=obstacle_avoidance_pb.bug2:main',
             
 
         ],
